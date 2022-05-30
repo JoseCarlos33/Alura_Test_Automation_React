374230bf9d856ff71ca602b3b138ac7af980b58d
José Carlos Noronha <josecarlosnoronha33@gmail.com>
2022-05-30 07:57:06 -0300

    :technologist: Add Financial Transaction Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import App from './App';
+import App, { calcularNovoSaldo } from './App';
 
 describe('Componente principal', () => {
   
   describe('Ao abrir app do banco', () => {
     
-
-    test('Mostrar o nome do banco', () => {
+    test('mostrar o nome do banco', () => {
       render(<App />);
       expect(screen.getByText('ByteBank')).toBeInTheDocument();
     })
 
-    test('Mostrar saldo', () => {
+    test('mostrar saldo', () => {
       render(<App />);
       expect(screen.getByText('Saldo:')).toBeInTheDocument();
     })
@@ -23,4 +22,18 @@ describe('Componente principal', () => {
       expect(screen.getByText('Realizar operação')).toBeInTheDocument();
     })
   })
+
+  describe('Ao realizar transação', () => {
+    test('de saque, o valor vai diminuir', () => {
+      const valores = {
+        transacao: 'saque',
+        valor: 100
+      }
+      
+      const novoSaldo = calcularNovoSaldo(valores, 1000);
+
+      expect(novoSaldo).toBe(900);
+    })
+  })
+
 })
