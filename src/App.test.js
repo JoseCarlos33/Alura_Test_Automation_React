193cee9e2488901fcf689c5e5e832ccabaa82a78
José Carlos Noronha <josecarlosnoronha33@gmail.com>
2022-06-01 08:31:43 -0300

    :sparkles: Add Behavior Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import App, { calcularNovoSaldo } from './App';
 
@@ -33,6 +33,22 @@ describe('Componente principal', () => {
       const novoSaldo = calcularNovoSaldo(valores, 1000);
 
       expect(novoSaldo).toBe(900);
+    })
+    test('que é um saque, a transação deve ser realizada', () => {
+      render(<App />);
+
+      const saldo = screen.getByText('R$ 1000');
+      const transacao =  screen.getByLabelText('Saque');
+      const valor = screen.getByTestId('valor');
+      const botaoTransacao = screen.getByText('Realizar operação');
+
+      expect(saldo.textContent).toBe('R$ 1000')
+
+      fireEvent.click(transacao, { target: { value: 'saque'}});
+      fireEvent.change(valor, { target: { value:  10 }});
+      fireEvent.click(botaoTransacao);
+
+      expect(saldo.textContent).toBe('R$ 990');
     })
   })
 
